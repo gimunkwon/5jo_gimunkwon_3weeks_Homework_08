b1a390c90cfb5e2449dcd2cb5180741e18080810
gimunkwon <gimon71@gmail.com>
2026-04-14 22:03:18 +0900

    [add] 언리얼 기초빌드 파일 추가

---
+++ Source/NBC_Dowork_08/NBC_Dowork_08.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "NBC_Dowork_08.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, NBC_Dowork_08, "NBC_Dowork_08" );

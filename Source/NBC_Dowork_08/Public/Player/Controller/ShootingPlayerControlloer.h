6f3eb46978359c9c7f1223168de0e500b1eb663e
gimunkwon <gimon71@gmail.com>
2026-04-18 16:29:50 +0900

    [None]

--- Source/NBC_Dowork_08/Public/Player/Controller/ShootingPlayerControlloer.h
+++ Source/NBC_Dowork_08/Public/Player/Controller/ShootingPlayerControlloer.h
@@ -4,8 +4,26 @@
 #include "GameFramework/PlayerController.h"
 #include "ShootingPlayerControlloer.generated.h"
 
+class UInputAction;
+class UInputMappingContext;
+
 UCLASS()
 class NBC_DOWORK_08_API AShootingPlayerControlloer : public APlayerController
 {
 	GENERATED_BODY()
+	
+public:
+	AShootingPlayerControlloer();
+	virtual void BeginPlay() override;
+	
+	UPROPERTY(EditAnywhere,BlueprintReadOnly, Category="Input")
+	TObjectPtr<UInputMappingContext> IMC_PlayerDefault;
+	UPROPERTY(EditAnywhere,BlueprintReadOnly,Category="Input")
+	TObjectPtr<UInputAction> IA_Look;
+	UPROPERTY(EditAnywhere,BlueprintReadOnly,Category="Input")
+	TObjectPtr<UInputAction> IA_Move;
+	UPROPERTY(EditAnywhere,BlueprintReadOnly,Category="Input")
+	TObjectPtr<UInputAction> IA_Jump;
+	UPROPERTY(EditAnywhere,BlueprintReadOnly,Category="Input")
+	TObjectPtr<UInputAction> IA_Shoot;
 };

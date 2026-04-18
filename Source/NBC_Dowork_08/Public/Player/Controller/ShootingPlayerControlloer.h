8b5e46bfc633867e0c11eefafb1aae9b1c3b0066
gimunkwon <gimon71@gmail.com>
2026-04-18 15:17:03 +0900

    [fix] PlayerSpringArmComp 수치 변경
    1인칭 시점을 위해 TargetArmLenght의 값을 0으로 초기화
    
    [add] PlayerController 클래스 추가

---
+++ Source/NBC_Dowork_08/Public/Player/Controller/ShootingPlayerControlloer.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "ShootingPlayerControlloer.generated.h"
+
+UCLASS()
+class NBC_DOWORK_08_API AShootingPlayerControlloer : public APlayerController
+{
+	GENERATED_BODY()
+};

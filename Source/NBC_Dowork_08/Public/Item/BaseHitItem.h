a0afdaeb05ce34484741d7b7ecc6eae92f1bec50
gimunkwon <gimon71@gmail.com>
2026-04-19 22:52:06 +0900

    [add] 플레이어가 맞출 아이템클래스들의 인터페이스 추가

---
+++ Source/NBC_Dowork_08/Public/Item/BaseHitItem.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Interface.h"
+#include "BaseHitItem.generated.h"
+
+
+UINTERFACE()
+class UBaseHitItem : public UInterface
+{
+	GENERATED_BODY()
+};
+
+/**
+ * 
+ */
+class NBC_DOWORK_08_API IBaseHitItem
+{
+	GENERATED_BODY()
+
+	
+public:
+};
